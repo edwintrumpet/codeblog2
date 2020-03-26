f21740163ea3778ed933ea968c67350b4f8292cc
edwin <spark.com.co@gmail.com>
2020-03-25 23:35:32 -0500

    Get data from filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: ['gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'posts',
+        path: `${__dirname}/content/posts/`,
+      },
+    },
+  ],
 };
