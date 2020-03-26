bba5240555a0b605e8a55d8f269a530858a1ea60
edwin <spark.com.co@gmail.com>
2020-03-25 22:43:43 -0500

    Set Layout component

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default function about() {
+  return (
+    <>
+      <h1>about works!</h1>
+      <Link to="/">Go index</Link>
+    </>
+  );
+}
